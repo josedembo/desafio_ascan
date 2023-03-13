c203179b3c45ed7f2d18faa087ca3d8d901c0795
josedembo <josedembo18@aluno.unilab.edu.br>
2023-03-13 07:54:55 -0300

    Cria o repositório da classe EventHistory
    A partir da clase criada será possivel gerenciar todos os metodos de
    conslta, criação, actualização e delecção dos registos dos eventos das
    subscrições dos usuários

---
+++ src/repositors/event_history.py
@@ -0,0 +1,25 @@
+from src.config.database.connection import DBConnectionHandler
+from src.entities.event_history import EventHistory
+
+
+class EventHistoryReporsitory:
+    
+    def create(self, subscription_id:int, type:str, created_at) -> None:
+        with DBConnectionHandler() as db:
+            event  = EventHistory(subscription_id=subscription_id, type=type, created_at=created_at)
+            db.session.add(event)
+            db.session.commit()
+            
+    def getAll(self) -> list:
+        with DBConnectionHandler() as db:
+            events = db.session.query(EventHistory).all()
+            return events
+    
+    def getBySubscriptionId(self, subscription_id:int):
+        with DBConnectionHandler() as db:
+            event = db.session.query(EventHistory).filter(EventHistory.subscription_id==subscription_id).first()
+            return event
+        
+    def delete(self, envent_id):
+        pass
+        
