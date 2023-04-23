46e6d8bbbc55f631decd25245f27030a5b14fd9a
josedembo <josedembo18@aluno.unilab.edu.br>
2023-04-23 06:12:56 -0300

    Adiciona o tratamento de exceções para o db
    Adicona o tratamento de erros para as chamadas no banco de dados para a
    tabela de usuário

--- src/repositors/user.py
+++ src/repositors/user.py
@@ -1,42 +1,77 @@
 from src.config.database.connection import DBConnectionHandler
 from src.entities.entities import User, Subscription, Status
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class UserRepositor:
     def create(self, email, password, username, full_name):
         with DBConnectionHandler() as db:
-            user = User(email=email, password=password, 
-                        username=username, full_name=full_name)
-            db.session.add(user)
-            db.session.commit()
+            try:
+                user = User(email=email, password=password, 
+                            username=username, full_name=full_name)
+                db.session.add(user)
+                db.session.commit()
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
             
     def getall(self):
         with DBConnectionHandler() as db:
-            data = db.session.query(User).all()
-            return data
+            try:
+                data = db.session.query(User).all()
+                return data
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
 
     def getById(self, id:int):
         with DBConnectionHandler() as db:
-            user = db.session.query(User).filter(User.id==id).first()
-            return user
+            try:
+                user = db.session.query(User).filter(User.id==id).first()
+                return user
+            except NoResultFound:
+                return None
+            except Exception as exception:
+                raise exception
         
     def getByEmail(self, email):
         with DBConnectionHandler() as db:
-            user = db.session.query(User).filter(User.email==email).first()
-            return user
+            try:
+                user = db.session.query(User).filter(User.email==email).first()
+                return user
+            except NoResultFound:
+                return None
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
         
     def getByUsername(self, username):
         with DBConnectionHandler() as db:
-            user = db.session.query(User).filter(User.username==username).first()
-            return user
-    
+            try:
+                user = db.session.query(User).filter(User.username==username).first()
+                return user
+            except NoResultFound:
+                return None
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+                
     def update(self, id:int, full_name):
         with DBConnectionHandler() as db:
-            db.session.query(User).filter(User.id==id).update({
-                "full_name":full_name 
-            })
-            db.session.commit()
+            try:
+                db.session.query(User).filter(User.id==id).update({
+                    "full_name":full_name 
+                })
+                db.session.commit()
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+            
     def delete(self, id:int):
         with DBConnectionHandler() as db:
-            db.session.query(User).filter(User.id==id).delete()
-            db.session.commit()
+            try:
+                db.session.query(User).filter(User.id==id).delete()
+                db.session.commit()
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
