4c02c081dd225cd58b402e619b1c15fac7fa89ce
josedembo <josedembo18@aluno.unilab.edu.br>
2023-05-10 14:04:10 -0300

    Modifica o prefixo do metodo de busca de dados

--- src/repositors/subscription.py
+++ src/repositors/subscription.py
@@ -26,7 +26,7 @@ class SubscriptionRepositor:
                 db.session.rollback()
                 raise ex
     
-    def getAll(self):
+    def select_all(self):
         with DBConnectionHandler() as db:
             try:
                 all_subscriptions = db.session.query(Subscription).all()
@@ -35,24 +35,24 @@ class SubscriptionRepositor:
                 db.session.rollback()
                 raise ex
     
-    def getById(self, subscription_id, user_id):
-        with DBConnectionHandler() as db:
-            try:
-                subscription = db.session.query(Subscription).filter(Subscription.id==subscription_id, Subscription.user_id==user_id).first()
-                return subscription
-            except NoResultFound:
-                return None
-            except Exception as ex:
-                db.session.rollback()
-                raise ex 
-        
-    def getByUserId(self, user_id:int):
+    def getById(self, subscription_id):
         with DBConnectionHandler() as db:
             try:
                 subscription = db.session.query(Subscription, EventHistory)\
                 .join(target=EventHistory, onclause=Subscription.id==EventHistory.subscription_id)\
-                .filter(Subscription.user_id==user_id).all()
+                .filter(Subscription.id==subscription_id).all()
                 return subscription
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+        
+    def select_by_Id(self, id):
+        with DBConnectionHandler() as db:
+            try:
+                subscription = db.session.query(Subscription).filter(Subscription.id==id).first()
+                return subscription
+            except NoResultFound:
+                return None
             except Exception as ex:
                 db.session.rollback()
                 raise ex
@@ -71,17 +71,20 @@ class SubscriptionRepositor:
     def select(self):
         with DBConnectionHandler() as db:
             try: 
-                subs_data = db.session.query(Subscription, EventHistory, Status )\
-                    .join(target=EventHistory,onclause=Subscription.id==EventHistory.subscription_id)\
+                subs_data = db.session.query(Subscription, Status )\
                     .join(target=Status, onclause=Subscription.status_id==Status.id)\
-                    .with_entities(
-                        Subscription.id,
-                        Subscription.created_at,
-                        Subscription.updated_at,
-                        EventHistory.type,
-                        EventHistory.created_at,
-                        Status.status_name
-                    ).all()
+                    .all()
+                return subs_data
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+    def select_by_status(self, status):
+        with DBConnectionHandler() as db:
+            try: 
+                subs_data = db.session.query(Subscription, Status )\
+                    .join(target=Status, onclause=Subscription.status_id==Status.id)\
+                    .filter(Subscription.status_id == status)\
+                    .all()
                 return subs_data
             except Exception as ex:
                 db.session.rollback()
